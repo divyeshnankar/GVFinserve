bda2c312178ef42180922ef84e6aadf6fa33265a
Aman Lodha <aml@narola.email>
2023-01-04 19:05:57 +0530

    Controller Created

---
+++ Controllers/ResourceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GVFinserve.Controllers
+{
+    public class ResourceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
